a6cadaddf8d028bfaca1fdc8c7aaddf3f1cf0902
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-12-29 18:59:37 +0700

    feat: implement findPeopleByName function to retrieve Person records by name

--- myApp.js
+++ myApp.js
@@ -37,7 +37,10 @@ const createManyPeople = (arrayOfPeople, done) => {
 };
 
 const findPeopleByName = (personName, done) => {
-	done(null /*, data*/);
+	Person.find({ name: personName }, (err, data) => {
+		if (err) return console.error(err);
+		done(null, data);
+	});
 };
 
 const findOneByFood = (food, done) => {
